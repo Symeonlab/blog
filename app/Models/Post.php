3e082cd60c260ea4b61d8573f4e04368f93546d8
symeonlampadarios <labadarios85@gmail.com>
2022-01-06 10:18:18 +0100

    initial

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $with = ['category','author'];
+
+
+    /**
+     * @return BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+      return $this->belongsTo(Category::class);
+    }
+
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+
+    /**
+     * Define the model's default state.
+     *
+     * @var string
+     */
+
+    protected $model = Post::class;
+
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
+            'slug' => $this->faker->slug,
+            'title' => $this->faker->sentence,
+            'excerpt' => $this->faker->sentence,
+            'body' => $this->faker->paragraph
+        ];
+    }
+}
