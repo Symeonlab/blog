55f1b0098cbb7850c09ec98a9b8f7f17b4c1f296
symeonlampadarios <labadarios85@gmail.com>
2022-01-08 11:01:35 +0100

    Merge Category and Search Queries

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,14 +12,14 @@ class PostController extends Controller
     {
 
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search','category']))->get(),
+            'posts' => Post::latest()->filter(request(['search','category','author']))->get(),
             'categories' => Category::all()
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post.show', [
+        return view('posts.show', [
             'post' => $post
         ]);
     }
